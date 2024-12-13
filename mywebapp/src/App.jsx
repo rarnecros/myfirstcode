975b768757d2892f17314b54855f3666d5603c0a
rarnecros <rarnecros306@gmail.com>
2024-12-13 16:42:06 +0500

    thjis is my first commit

---
+++ mywebapp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>this is my react app</h1>
+     <button>submit</button>
+     <button>login</button>
+     
+    </>
+  )
+}
+
+export default App
---
+++ mywebapp/src/main.py
@@ -0,0 +1,12 @@
+calculator
+print("1 add")
+print("2 subtract")
+print("3 multiply")
+print("4 divide")
+option = int(input("choose an operation": ))
+if(the operation in [1,2,3,4]):
+    num1 = int(input("enter the 1st number:  "))
+    num2 = int(input("enter the 2nd number:  "))git config --global user.name "YourGitHubUsername"
+git config --global user.email "YourEmail@example.com"
+git remote add origin https://github.com/username/repo.git
+git remote set-url origin https://TOKEN@github.com/username/repo.git
